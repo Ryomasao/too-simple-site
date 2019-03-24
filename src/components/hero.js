3322866e3a88b7d63b7d7df69f8acb2d837ff1c2
tohu.masao <konoemario@gmail.com>
2019-03-24 13:46:39 +0900

    slick

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,10 +1,30 @@
 import React from "react"
 import mv from "../images/main.png"
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 
-const Hero = () => (
-  <div>
-    <img src={mv} alt="mv" />
-  </div>
-)
+class Hero extends React.Component {
+  render() {
+    var settings = {
+      dots: false,
+      infinite: true,
+      speed: 800,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+    }
+    return (
+      <Slider {...settings}>
+        <div>
+          <img src={mv} alt="mv" />
+        </div>
+        <div>
+          <img src={mv} alt="mv" />
+        </div>
+      </Slider>
+    )
+  }
+}
 
 export default Hero
